58ba5533ddb07ce5c39433208e9fde33c98da4e7
Jorge Gil <jagilren@gmail.com>
2022-06-26 14:57:40 -0500

    Commmit directamente desde PyCharm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 26 de Junio de 2022 14:28
 # Nuevo Cambio
 # se adiciona branch main
-# Vamos a hacer commit des del Version Control de PyCharm
+# Vamos a hacer commit des del Version Control de PyCharm 2.0
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
